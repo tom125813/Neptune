7467a4337558a8b4cb27de61505cc806d744424e
tom125813 <thomasrayner16@gmail.com>
2022-06-09 17:33:24 +0100

    range

--- src/main/java/com/doontcare/me/managers/ProfileManager.java
+++ src/main/java/com/doontcare/me/managers/ProfileManager.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 
 public class ProfileManager {
 
-    public HashMap<UUID, Profile> activeProfiles = new HashMap<UUID, Profile>();
+    private HashMap<UUID, Profile> activeProfiles = new HashMap<UUID, Profile>();
 
     public void add(UUID uuid, Profile profile) {activeProfiles.put(uuid,profile);}
     public Profile get(UUID uuid) {return activeProfiles.get(uuid);}
